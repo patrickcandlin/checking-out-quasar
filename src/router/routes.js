efb1b5bbeab3ab42c42c661dff718b4784883c95
“Patrick <pctennislessons@gmail.com>
2020-02-05 21:41:30 -0700

    c

---
+++ src/router/routes.js
@@ -0,0 +1,22 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/Index.vue") }]
+  },
+  {
+    path: "/apps",
+    component: () => import("layouts/Appetizers.vue"),
+    children: [{ path: "appetizers", component: () => import("pages/Index.vue") }]
+  }
+];
+
+// Always leave this as last one
+if (process.env.MODE !== "ssr") {
+  routes.push({
+    path: "*",
+    component: () => import("pages/Error404.vue")
+  });
+}
+
+export default routes;
